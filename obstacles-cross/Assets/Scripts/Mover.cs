a0aaa761256383eec26b7f74847988a14e5077e7
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-02-09 19:00:01 +0600

    methods refactoring and restructuring

--- obstacles-cross/Assets/Scripts/Mover.cs
+++ obstacles-cross/Assets/Scripts/Mover.cs
@@ -6,36 +6,44 @@ public class Mover : MonoBehaviour
 
 
 
-    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float moveSpeed = 10f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        PrintInstruction();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; //-1 pushing left, 1 pushing right, 0 is nothing 
-        float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
-        transform.Translate(xValue, yValue, zValue);
-        
+         
+        MovePlayer();
+       
     }
 
     void PrintInstruction()
     {
         Debug.Log("You are welcome to our game!");
+        Debug.Log("Move using wasd or arrow keys.");
+        Debug.Log("Don't hit the walls!");
         
     }
 
 
 
+    void MovePlayer() 
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; //-1 pushing left, 1 pushing right, 0 is nothing 
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, yValue, zValue);
+    }
+    
 
 
 }
