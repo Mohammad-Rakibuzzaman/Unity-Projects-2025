11898be764e0177c441e2637e33693e8d50682c7
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-02-10 17:14:19 +0600

    add a GetCommponent

--- obstacles-cross/Assets/Scripts/ObjectHit.cs
+++ obstacles-cross/Assets/Scripts/ObjectHit.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    //OnCollisionEnter is a callback method or function that is called when the object collides with another object
     private void OnCollisionEnter(Collision other) 
     {
+        GetComponent<MeshRenderer>().material.color = Color.white;
         Debug.Log("You hit something!");
     }
+
+
+
+
 }
