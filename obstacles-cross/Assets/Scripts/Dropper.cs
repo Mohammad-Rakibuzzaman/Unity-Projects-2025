28e5dd7e798b00d489447a663b0252cd16a67ff2
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-02-12 04:20:46 +0600

    add variable to my dropper.cs script

--- obstacles-cross/Assets/Scripts/Dropper.cs
+++ obstacles-cross/Assets/Scripts/Dropper.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+
+    [SerializeField] float timeToWait = 3.0f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,11 @@ public class Dropper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Time.time);
+
+        // Debug.Log(Time.time);
+        if (Time.time > timeToWait) 
+        {
+            Debug.Log("Lookout below!");
+        }
     }
 }
