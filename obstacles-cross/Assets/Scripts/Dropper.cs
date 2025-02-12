28ad4f97a508561e0e10fe8c79de35754e87339d
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-02-13 04:11:58 +0600

    caching a reference done

--- obstacles-cross/Assets/Scripts/Dropper.cs
+++ obstacles-cross/Assets/Scripts/Dropper.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
 
-    [SerializeField] float timeToWait = 3.0f;
+    [SerializeField] float timeToWait = 10f;
+
+    MeshRenderer myMeshRenderer;
+    Rigidbody myRigidbody;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        //This is called caching a reference to the MeshRenderer component
+        myMeshRenderer = GetComponent<MeshRenderer>();
+        // GetComponent<MeshRenderer>().enabled = false;
+        myRigidbody = GetComponent<Rigidbody>();
 
+        myMeshRenderer.enabled = false;
+        myRigidbody.useGravity = false;
+    }
+ 
     // Update is called once per frame
     void Update()
     {
@@ -19,7 +28,9 @@ public class Dropper : MonoBehaviour
         // Debug.Log(Time.time);
         if (Time.time > timeToWait) 
         {
-            Debug.Log("Lookout below!");
+            myMeshRenderer.enabled = true;
+            myRigidbody.useGravity = true;
+            // Debug.Log("Lookout below!");
         }
     }
 }
